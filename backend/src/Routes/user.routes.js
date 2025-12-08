df0b44ce757f0eaf05d83e04e5f2c7f671cc3526
hosurVijay <hosurvijayendra@gmail.com>
2025-12-08 23:41:57 +0530

    User controller Route added 81225

---
+++ backend/src/Routes/user.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+  registerUser,
+  login,
+  logOut,
+  sendOtp,
+  updatePhoneNumber,
+  updateEmail,
+  updatePassword,
+  changePasswordWithOTP,
+} from "../Controller/user.controller.js";
+import { verifyUser } from "../Middleware/auth.middleware.js";
+const router = Router();
+
+router.route("/register").post(verifyUser, registerUser);
+router.route("/login").post(verifyUser, login);
+router.route("/logout").post(verifyUser, logOut);
+router.route("/update/email").post(verifyUser, updateEmail);
+router.route("/update/phone-number").post(verifyUser, updatePhoneNumber);
+router.route("/update/password/otp").post(verifyUser, changePasswordWithOTP);
+router.route("/update/password").post(verifyUser, updatePassword);
+router.route("/send/otp").post(sendOtp);
+
+export default router;
