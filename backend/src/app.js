ca39b88c5d71cf3678adffa99b7b81e515531018
hosurVijay <hosurvijayendra@gmail.com>
2025-12-08 23:43:37 +0530

    User route added 81225

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,4 +15,7 @@ app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// User controller Routes
+import userRoute from "./Routes/user.routes.js";
+app.use("/api/v1/user", userRoute);
 export { app };
